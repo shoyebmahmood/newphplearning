c4821c4e18b9f046560a7870430fceec83cf2941
shoyebmahmood <mahmoodshoyeb@gmail.com>
2020-04-01 18:26:26 +0600

    ADD NEW CODE

--- foreach.php
+++ foreach.php
@@ -1,5 +1,10 @@
 <?php
 $array = array("Shoyeb", "Nuuha", "Prottush", "Kaniz");
 foreach ($array as $name) {
-    echo $name;
+    echo $name . PHP_EOL;
+}
+echo PHP_EOL;
+$days = array("Saurday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+foreach ($days as $day) {
+    echo $day . PHP_EOL;
 }
