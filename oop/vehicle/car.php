917993b8ba4a82519ced86c2e342d92025c35728
shoyebmahmood <mahmoodshoyeb@gmail.com>
2020-04-27 16:26:59 +0600

    it's time to implement the abstract start() method in the subclasses. A car has its own way of starting its engine – you need to place the key in the ignition. In Car.php, add a private property, $hasKeyinIgnition, along with the start() implementation So, a car implements the engine start with the key in the ignition. $this->hasKeyinIgnition should be true to set the $engineStatus variable to start or true.

--- oop/vehicle/car.php
+++ oop/vehicle/car.php
@@ -1,12 +1,19 @@
 <?php
 require_once 'vehicle1.php';
 
-class Car extends Vehicle
+class Car extends AbstractVehicle
 {
     public $doors = 4;
     public $passengerCapacity = 5;
     public $steeringWheel = true;
     public $transmission = 'Manual';
+    private $hasKeyinIgnition = true;
+    public function start()
+    {
+        if ($this->hasKeyinIgnition) {
+            $this->engineStatus = true;
+        }
+    }
 }
 /*
 $car = new Car('Honda', 'Civic', 'Red', 4, '23CJ4567');
@@ -21,8 +28,16 @@ echo "Passenger Capacity: " . $car->passengerCapacity . PHP_EOL;
 echo "Steering Wheel: " . $car->steeringWheel . PHP_EOL;
 echo "Transmission: " . $car->transmission . PHP_EOL;
 */
-$car1 = new Car('Honda', 'Civic', 'Red', 4, '23CJ4567');
+/*$car1 = new Car('Honda', 'Civic', 'Red', 4, '23CJ4567');
 $car2 = new Car('Toyota', 'Allion', 'White', 4, '24CJ4568');
 $car3 = new Car('Hyundai', 'Elantra', 'Black', 4, '24CJ1234');
 $car4 = new Car('Chevrolet', 'Camaro', 'Yellow', 4, '23CJ9397');
 echo "Available cars are " . Car::$counter . PHP_EOL;
+*/
+$car = new Car('Honda', 'Civic', 'Red', 4, '23CJ4567');
+$car->start();
+echo "The car is " . ($car->getEngineStatus() ?
+    'running' : 'stopped') . PHP_EOL;
+$car->stop();
+echo "The car is " . ($car->getEngineStatus() ?
+    'running' : 'stopped') . PHP_EOL;
